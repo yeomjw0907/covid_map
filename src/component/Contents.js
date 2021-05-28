7bf1500d3f43c6d8c8bb018d8e3d8fdd2ea7c1f2
gardenyeom <{yeomjw0907@naver.com}>
2021-05-29 03:39:55 +0900

    17분부터

---
+++ src/component/Contents.js
@@ -0,0 +1,14 @@
+import {useState, useEffect} from 'react'
+
+const Contents = () => {
+    return (
+        <section className='contents'>
+            <h2>현황</h2>
+            <div>
+
+            </div>
+        </section>
+    )
+}
+
+export default Contents
