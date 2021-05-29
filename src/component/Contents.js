964aa016b1051e19163360571c261b83889aec80
gardenyeom <{yeomjw0907@naver.com}>
2021-05-30 02:58:16 +0900

    get_daily_api

--- src/component/Contents.js
+++ src/component/Contents.js
@@ -1,6 +1,17 @@
 import {useState, useEffect} from 'react'
-
+import axios from 'axios'
+import { Bar, Doughnut, Line } from "react-chartjs-2"
 const Contents = () => {
+    useEffect(()=>{
+        const fetchEvent = () => {
+
+        }
+        fetchEvent = async () => {
+            const res = await axios.get("https://api.covid19api.com/total/dayone/country/kr")
+            console.log(res)
+        }
+
+    })
     return (
         <section className='contents'>
             <h2>현황</h2>
